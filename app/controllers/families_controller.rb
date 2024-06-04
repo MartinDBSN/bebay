4961dd04e6443174fa50d0b744c10d9dbbcf18aa
Hugo Nieuwenhuyse <hugo.nieuwenhuyse@gmail.com>
2024-06-04 15:37:41 +0200

    Done for the index

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,15 @@
-class FamiliesController < ApplicationController
+class UsersController < ApplicationController
+#index method to adapt when doing the dates query part.
+  def index
+    @users = User.all
+    # if params[:query].present?
+    #   sql_subquery = <<~SQL
+    #     available_dates.category ILIKE :query
+    #     OR users.home_address ILIKE :query
+    #     OR available_dates.start_date ILIKE :query
+    #     OR available_dates.end_date ILIKE :query
+    #   SQL
+    #   @users = @users.joins(:available_dates).where(sql_subquery, query: "%#{params[:query]}%")
+    # end
+  end
 end
