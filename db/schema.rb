aa52ed2211d221a142d38c7880ee021f521660c1
Martin Dubuisson <martin.dbssn@gmail.com>
2024-06-04 11:48:18 +0200

    Models and controllers creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_144337) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_04_092106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "available_dates", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "category"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_available_dates_on_family_id"
+  end
+
+  create_table "children", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.date "birthday"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_children_on_family_id"
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string "family_name"
+    t.string "home_address"
+    t.integer "point_count"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_families_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_144337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "available_dates", "families"
+  add_foreign_key "children", "families"
+  add_foreign_key "families", "users"
 end
