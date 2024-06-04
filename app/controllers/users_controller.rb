a714479b5d0331cc96d96e95d03f8c9b4b0f298d
Hugo Nieuwenhuyse <hugo.nieuwenhuyse@gmail.com>
2024-06-04 16:39:01 +0200

    done first version show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,9 @@ class UsersController < ApplicationController
     #   SQL
     #   @users = @users.joins(:available_dates).where(sql_subquery, query: "%#{params[:query]}%")
     # end
+    def show
+      @user = User.find(params[:id])
+      # @booking = Booking.new
+    end
   end
 end
