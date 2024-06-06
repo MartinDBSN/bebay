278320ccccfed358a5b57619945d16280dfc9e87
Kenta Merry <merryk69@gmail.com>
2024-06-06 17:42:41 +0200

    Cloudinary succesfully setup on families and children, modified seed to display images for existing families

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,6 @@ class Child < ApplicationRecord
   belongs_to :user
   validates :name, :birthday, presence: true
   validates :child_comment, length: { maximum: 160 }, allow_blank: true
+
+  has_one_attached :child_photo
 end
