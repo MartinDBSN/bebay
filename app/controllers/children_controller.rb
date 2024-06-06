899e63ab3dc232d9dcc75a3e50b9044c6a62a5d2
Kenta Merry <merryk69@gmail.com>
2024-06-06 11:47:21 +0200

    Functional child registration form for creating new Child after User/Family signup

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -4,6 +4,18 @@ class ChildrenController < ApplicationController
   end
 
   def create
-    # @child = Child.create
+    @child = Child.new(child_params)
+    @child.user = current_user
+    if @child.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity, notice: "Sorry, we couldn't register your child. Make sure you filled the form correctly!"
+    end
+  end
+
+  private
+
+  def child_params
+    params.require(:child).permit(:name, :gender, :birthday, :child_comment)
   end
 end
