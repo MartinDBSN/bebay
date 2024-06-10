8d7f8dee5aab5ca3a662ec3b31406035eb56fb53
Hugo Nieuwenhuyse <hugo.nieuwenhuyse@gmail.com>
2024-06-10 11:27:36 +0200

    modified model children pool and relatives

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :child
   has_many :children_pools
+  has_many :children, through: :children_pools
 
   validates :start_date, presence: true
   #validates :end_date, comparison: { greater_than: :start_date }
