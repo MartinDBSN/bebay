e1d31e5ef90d599abf732389c27056a536d28086
Hugo Nieuwenhuyse <hugo.nieuwenhuyse@gmail.com>
2024-06-10 15:21:00 +0200

    Done for the children pool association

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,30 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :child
   has_many :children_pools
   has_many :children, through: :children_pools
 
   validates :start_date, presence: true
   #validates :end_date, comparison: { greater_than: :start_date }
-  validate :dates_cannot_overlap
 
   def period
     start_date..end_date
-  end
-
-  private
-
-  def dates_cannot_overlap
-    @bookings = Booking.all.where(user: user)
-    @availabilities = AvailableDate.all.where(user: user)
-
-    is_overlapping = @bookings.any? do |booking|
-      if booking.start_date.day != start_date.day
-        return true
-      else
-        return false
-      end
-    end
-    errors.add(:overlaps_with_other) if is_overlapping
   end
 end
