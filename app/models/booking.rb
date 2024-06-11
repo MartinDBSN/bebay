3abf3758537597bbc4f71a37b3deaa30bfdd6f77
Hugo Nieuwenhuyse <hugo.nieuwenhuyse@gmail.com>
2024-06-11 18:12:18 +0200

    modal view family

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   has_many :children, through: :children_pools, dependent: :destroy
 
   validates :start_date, presence: true
+  validates :category, presence: true
   #validates :end_date, comparison: { greater_than: :start_date }
 
   def period
